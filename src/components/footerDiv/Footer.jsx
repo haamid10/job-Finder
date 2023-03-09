a1c7eeebf43318213036c4ee926c438a795e0f74
haamid10 <apdixamiid919@gmail.com>
2023-03-09 23:14:10 +0300

    footer

--- src/components/footerDiv/Footer.jsx
+++ src/components/footerDiv/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-5 m-auto items-center justify-center'>
+    <div className=' footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-5 m-auto items-center justify-center'>
 
       <div>
         <div className='logoDiv'>
@@ -10,6 +10,46 @@ const Footer = () => {
         </div>
         <p className='text-white pb-[13px] opacity-70 leading-7'> We always make our seekers and companies find the best jobs ans employers find the best candidates</p>
       </div>
+
+      <div className=' grid '>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white '> company</span>
+
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[.7] hover:opacity-1'>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>Features</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>News</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>FAQ</li>
+
+        </div>
+
+      </div>
+
+      <div className=' grid '>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white '> company</span>
+
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[.7] hover:opacity-1'>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>Features</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>News</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>FAQ</li>
+
+        </div>
+
+      </div>
+
+      <div className=' grid '>
+        <span className='divTitle text-[18px] font-semibold pb-[1.5rem] text-white '> company</span>
+
+        <div className='grid gap-3'>
+          <li className='text-white opacity-[.7] hover:opacity-1'>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>Features</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>News</li>
+          <li className='text-white opacity-[.7] hover:opacity-1'>FAQ</li>
+
+        </div>
+
+      </div>
+      
     </div>
   )
 }
