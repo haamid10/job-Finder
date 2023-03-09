3bd9cf5838e76ac6c0df648cb12f67068ba0e42c
haamid10 <apdixamiid919@gmail.com>
2023-03-09 22:54:14 +0300

    begun footer

--- src/components/footerDiv/Footer.jsx
+++ src/components/footerDiv/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className=' footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-[8] grid grid-cols-5 m-auto items-center justify-center'>
+
+      <div>
+        <div className='logoDiv'>
+          <h1 className=' logo text-[25px text-white pb-[1.5rem]'> <strong>Job</strong>Finder</h1>
+        </div>
+        <p className='text-white pb-[13px] opacity-70 leading-7'> We always make our seekers and companies find the best jobs ans employers find the best candidates</p>
+      </div>
     </div>
   )
 }
